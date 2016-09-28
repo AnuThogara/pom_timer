de3c66073e9384ff67ea1fb0783654bbf0e100e8
AnuThogara <anuradha.thogara@gmail.com>
2016-09-28 19:38:34 -0400

    modified js file

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,39 @@
-var age = promt('how old r u?');
+(function() {
+    //initialize variables
+    var startButton = $("#start");
+
+    var seconds = $('#seconds');
+    // main functionaility
+
+    startButton.on('click', countdown);
+
+    //function definition
+    function countdown() {
+     var secondsText= seconds.text();
+     var secondsTextAsNumber= parseInt(secondsText);
+     if(secondsTextAsNumber === 0){
+       seconds.text("59");
+       //then change sec text to 59
+
+     }
+     else{
+       // var secondsvalue = parseInt(seconds.text());
+       //
+       // seconds.text(pad(secondsvalue - 1));
+
+     var decreasedSecondsAsNumberByOne = secondsTextAsNumber - 1;
+     var padSecondsTextAsNumber = pad(secondsTextAsNumber);
+     seconds.text(padSecondsTextAsNumber);
+}
+    }
+
+    function pad(num) {
+        if (num < 10) {
+            //spit out num leading 0
+            return "0" + num;
+        } else {
+            //spit out the original number
+            return num;
+        }
+    }
+}());
